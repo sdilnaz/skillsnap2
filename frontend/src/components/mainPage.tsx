dc263cf103c9b778334f85c9f9888a5c6cf426aa
sdilnaz <dilnaz.sembekova@gmail.com>
2024-08-01 01:25:30 +0500

    Update mainPage.tsx

--- frontend/src/components/mainPage.tsx
+++ frontend/src/components/mainPage.tsx
@@ -3,19 +3,16 @@ import { Button } from "@/components/ui/button";
 
 export default function PreviewMainPage() {
   return (
-    <div className="flex items-center  ">
-  <section className="w-1/3 py-2">
-    <div className="bg-transparent">
-      <div className="flex flex-col items-start gap-4  lg:block">
-        
-        <p className="text-white ">
-          Совершенствуй свои навыки в фотографии и повышай качество своей работы благодаря детальной обратной связи. Загрузи свои фотографии для получения персональных рекомендаций.
-        </p>
-        
-      </div>
+    <div className="hidden lg:flex items-center">
+      <section className="w-1/3 py-2">
+        <div className="bg-transparent">
+          <div className="flex-col items-start gap-4 hidden lg:flex">
+            <p className="text-white">
+              Совершенствуй свои навыки в фотографии и повышай качество своей работы благодаря детальной обратной связи. Загрузи свои фотографии для получения персональных рекомендаций.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
-  </section>
-</div>
-
   );
 }
